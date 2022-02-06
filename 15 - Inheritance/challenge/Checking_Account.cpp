bd84befcbd908cea4614da172d3e3d7bb9d66d10
pviscone <pieroviscone@gmail.com>
2022-02-06 16:12:42 +0100

    Inheritance challenge DONE

---
+++ 15 - Inheritance/challenge/Checking_Account.cpp
@@ -0,0 +1,10 @@
+#include "Checking_Account.h"
+#include "Account.h"
+#include <string>
+
+bool Checking_Account::withdraw(double amount){
+    amount=amount-fee;
+    return Account::withdraw(amount);
+}
+Checking_Account::Checking_Account(std::string name, double balance):
+    Account{name,balance}{}
